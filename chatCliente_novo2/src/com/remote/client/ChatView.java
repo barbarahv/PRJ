5b9a518e088775ac71fe26ea64ba6655be5c5d46
Bárbara Hansen de Vasconcelos <98177134+barbarahv@users.noreply.github.com>
2023-06-14 17:26:13 -0300

    Add files via upload

---
+++ chatCliente_novo2/src/com/remote/client/ChatView.java
@@ -0,0 +1,353 @@
+package com.remote.client;
+
+import com.remote.server.InterfaceServer;
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.rmi.RemoteException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+public class ChatView extends javax.swing.JFrame implements Runnable{
+    private ChatClient client;
+    private InterfaceServer server;
+    private DefaultListModel<String> model = new DefaultListModel<>();
+    private Vector<String> listClients;
+    private String name;
+    
+    
+    public ChatView(String name,String autorization,InterfaceServer server) {
+        initComponents();
+        
+        this.server = server;
+        this.name = name;
+        
+        
+        if(autorization.equals("Administrador")){
+            System.out.print(autorization);
+            listConnect.setComponentPopupMenu(jPopupMenu1);
+        }
+        
+        this.setLocationRelativeTo(null);
+        this.setTitle("Usuário (" + name + ")");
+               
+       
+        this.addWindowListener(new java.awt.event.WindowAdapter() {    
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(new JFrame(), 
+                    "Tem quer sair ?", "Sair?", 
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+                    try {
+                        server.removeClient(name);
+                    } catch (RemoteException ex) {
+                        System.out.println("Error: " + ex.getMessage());
+                    }
+                    System.exit(0);
+                }else{
+                   
+                }
+            }
+        });
+        
+        
+        inputMsg.setForeground(Color.GRAY);
+        inputMsg.setText("Entre com a sua mensagem ...");
+        inputMsg.addFocusListener(new FocusListener() {
+        @Override
+         public void focusGained(FocusEvent e) {
+            if (inputMsg.getText().equals("Entre com a sua mensagem ...")) {
+                inputMsg.setText("");
+                inputMsg.setForeground(Color.BLACK);
+            }
+        }
+        @Override
+         public void focusLost(FocusEvent e) {
+            if (inputMsg.getText().isEmpty()) {
+                inputMsg.setForeground(Color.GRAY);
+                inputMsg.setText("Entre com a sua mensagem  ...");
+            }
+        }
+        });
+        
+        listClients = new Vector<>();
+        listConnect.setListData(listClients);
+        
+        try{
+            client = new ChatClient(name,server,inputMsg,listMessage,jPanel1);
+        } catch (RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        
+        
+        Timer minuteur = new Timer();
+        TimerTask tache = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    int[] indices = listConnect.getSelectedIndices();
+                    model.clear();
+                    listClients = server.getListClientByName(name);
+                    int i=0;
+                    while(i<listClients.size()){
+                        model.addElement(listClients.get(i));
+                        i++;
+                    }
+                    listConnect.setModel(model);
+                    listConnect.setSelectedIndices(indices);
+                } catch (RemoteException ex) {
+                    System.out.println("Error: " + ex.getMessage());
+                }
+            }
+        };
+        minuteur.schedule(tache,0,20000);
+    }
+    
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listConnect = new javax.swing.JList<>();
+        btnSend = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        listMessage = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        inputMsg = new javax.swing.JTextArea();
+
+        jMenuItem1.setText("Remove Users");
+        jMenuItem1.setActionCommand("");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jPopupMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Block Users");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jPopupMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Reactive Users");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jPopupMenu1.add(jMenuItem3);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+        setResizable(false);
+
+        listConnect.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        listConnect.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        listConnect.setToolTipText("");
+        jScrollPane1.setViewportView(listConnect);
+
+        btnSend.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        btnSend.setText("Enviar");
+        btnSend.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSendActionPerformed(evt);
+            }
+        });
+
+        listMessage.setEditable(false);
+        listMessage.setColumns(20);
+        listMessage.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        listMessage.setRows(5);
+        listMessage.setRequestFocusEnabled(false);
+        jScrollPane3.setViewportView(listMessage);
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 0, 16)); // NOI18N
+        jLabel2.setText("Usuários Onlines");
+
+        jButton1.setText("Atualizar");
+        jButton1.setActionCommand("");
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jPanel1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 473, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 113, Short.MAX_VALUE)
+        );
+
+        jScrollPane4.setViewportView(jPanel1);
+
+        inputMsg.setColumns(20);
+        inputMsg.setRows(5);
+        inputMsg.setToolTipText("Enter your Message ...");
+        inputMsg.setMargin(new java.awt.Insets(6, 0, 0, 16));
+        jScrollPane2.setViewportView(inputMsg);
+        inputMsg.getAccessibleContext().setAccessibleName("Enter your Message ...");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane3)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSend, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 11, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(27, 27, 27)
+                                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton1)
+                                .addGap(12, 12, 12)
+                                .addComponent(btnSend, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
+        if(!inputMsg.getText().equals("")){
+            if(!inputMsg.getText().equals("Entre com a sua mensagem  ...")){
+                client.sendMessage(listConnect.getSelectedValuesList());
+                inputMsg.setText("");
+            }else{
+            JOptionPane.showMessageDialog(this,"Por favor, insira algo para definir sua mensagem","Alerta",JOptionPane.WARNING_MESSAGE);
+        }
+        }else{
+            JOptionPane.showMessageDialog(this,"Por favor, insira algo para definir sua mensagem","Alerta",JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_btnSendActionPerformed
+
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        Thread thread = new Thread(this);
+        thread.start();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+   
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        try {
+            server.removeClient(listConnect.getSelectedValuesList());
+        } catch (RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } 
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        try {
+            server.blockClient(listConnect.getSelectedValuesList());
+        } catch (RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        try {
+            server.reactiveClient(listConnect.getSelectedValuesList());
+        } catch (RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+   
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSend;
+    private javax.swing.JTextArea inputMsg;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPopupMenu jPopupMenu1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JList<String> listConnect;
+    private javax.swing.JTextArea listMessage;
+    // End of variables declaration//GEN-END:variables
+
+   
+    @Override
+    public void run() {
+        try {
+            
+            model.clear();
+            listClients = server.getListClientByName(name);
+            int i=0;
+            while(i<listClients.size()){
+                model.addElement(listClients.get(i));
+                i++;
+            }
+            listConnect.setModel(model);
+        } catch (RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+}
---
+++ chatCliente_novo2/src/com/remote/client/LoginView.java
@@ -0,0 +1,143 @@
+package com.remote.client;
+
+import com.remote.server.InterfaceServer;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class LoginView extends javax.swing.JFrame {
+    private InterfaceServer server;
+    
+    public LoginView() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        
+        try {
+            server = (InterfaceServer) Naming.lookup("rmi://localhost:4321/remote");
+        } catch (NotBoundException | MalformedURLException | RemoteException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btConectar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Log in");
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setResizable(false);
+
+        btConectar.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        btConectar.setText("Conectar");
+        btConectar.setToolTipText("");
+        btConectar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btConectar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btConectarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel1.setText("Usuário:");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Usuário", "Administrador" }));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(82, 82, 82)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btConectar, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                            .addComponent(jComboBox1, 0, 0, Short.MAX_VALUE))))
+                .addContainerGap(38, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btConectar)
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    private void btConectarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btConectarActionPerformed
+        try {
+            if(!server.checkUsername(jTextField1.getText())){
+                if(!jTextField1.getText().equals("") && !jTextField1.getText().contains(" ")){
+                    new ChatView(jTextField1.getText(),(String)jComboBox1.getSelectedItem(),server).setVisible(true);
+                    this.dispose();
+                }
+            }else{
+                JOptionPane.showMessageDialog(new JFrame(),"Esse usuario ja existe","Alerta",JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (RemoteException ex) {
+            JOptionPane.showMessageDialog(this,"Error!! ocorreu um remoteException no servidor. \\n\\ntente: \\n- Reiniciar o servidor \\n- Alterar a porta","Alerta ",JOptionPane.WARNING_MESSAGE);
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }//GEN-LAST:event_btConectarActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginView().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btConectar;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
